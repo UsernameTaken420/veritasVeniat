b51beefed104b288c4660af709b16447f5be8557
UsernameTaken420 <Ngabin1999@gmail.com>
2026-02-28 19:40:42 -0300

    Chore: ran terraform fmt

--- main.tf
+++ main.tf
@@ -1,43 +1,43 @@
 terraform {
-	required_providers {
-		aws = {
-			source = "hashicorp/aws"
-			version = "~> 5.0"
-		}
-	}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 provider "aws" {
-	region = "us-east-1"
+  region = "us-east-1"
 }
 
 data "external" "env" {
-	program = ["${path.module}/env.sh"]
+  program = ["${path.module}/env.sh"]
 }
 
 resource "aws_amplify_app" "veritasveniat" {
-	name = "veritasveniat"
-	repository = "https://github.com/UsernameTaken420/veritasVeniat"
-	platform = "WEB"
-	access_token = data.external.env.result["GH_TOKEN"]
-	custom_rule {
-		source = "/<*>"
-		status = "404"
-		target = "/404"
-	}
+  name         = "veritasveniat"
+  repository   = "https://github.com/UsernameTaken420/veritasVeniat"
+  platform     = "WEB"
+  access_token = data.external.env.result["GH_TOKEN"]
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/404"
+  }
 
-	custom_rule {
-		source = "https://veritasveniat.com"
-		status = 302
-		target = "https://blog.veritasveniat.com"
-	}
-	custom_rule {
-		source = "https://www.veritasveniat.com"
-		status = 302
-		target = "https://blog.veritasveniat.com"
-	}
-	
-	build_spec = <<-EOT
+  custom_rule {
+    source = "https://veritasveniat.com"
+    status = 302
+    target = "https://blog.veritasveniat.com"
+  }
+  custom_rule {
+    source = "https://www.veritasveniat.com"
+    status = 302
+    target = "https://blog.veritasveniat.com"
+  }
+
+  build_spec = <<-EOT
 version: 1
 frontend:
   phases:
@@ -61,25 +61,25 @@ frontend:
 }
 
 resource "aws_amplify_branch" "master" {
-	app_id = aws_amplify_app.veritasveniat.id
-	branch_name = "master"
-	stage = "PRODUCTION"
-	framework = "Hugo"
-	enable_auto_build = true
+  app_id            = aws_amplify_app.veritasveniat.id
+  branch_name       = "master"
+  stage             = "PRODUCTION"
+  framework         = "Hugo"
+  enable_auto_build = true
 
-	depends_on = [ resource.aws_amplify_app.veritasveniat ]
+  depends_on = [resource.aws_amplify_app.veritasveniat]
 }
 
 resource "time_sleep" "wait_40_seconds" {
-	depends_on = [ resource.aws_amplify_app.veritasveniat ]
-	create_duration = "40s"
+  depends_on      = [resource.aws_amplify_app.veritasveniat]
+  create_duration = "40s"
 }
 
 resource "aws_amplify_domain_association" "domainAssociation" {
-	app_id = aws_amplify_app.veritasveniat.id
-	domain_name = "blog.veritasveniat.com"
-	sub_domain {
-		branch_name = aws_amplify_branch.master.branch_name
-		prefix = ""
-	}
+  app_id      = aws_amplify_app.veritasveniat.id
+  domain_name = "blog.veritasveniat.com"
+  sub_domain {
+    branch_name = aws_amplify_branch.master.branch_name
+    prefix      = ""
+  }
 }
